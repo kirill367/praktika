b88e1e5d47f07078fade8d465150137c3ffe2491
kirill367 <kirill.antipov367@rambler.ru>
2021-06-28 15:16:11 +0300

    zeroes to avarage done
    to do: refactor useful cycle

--- main.py
+++ main.py
@@ -7,22 +7,34 @@ def main():
     pd.set_option("display.max_rows", 10000)
     df = pd.read_csv('sensors_sample_data.csv', delimiter=';')
 
-# 9105 - 16.���  были подоюные записи
+# 9105 - 16.���  были подобные записи в стобце velocity, заменяем на нули
     numOfSrtangeSign = 0
+    lineswithstrangesign = []
     for i in df.velocity:
         for j in i:
             if j == '�':
                 df.velocity[numOfSrtangeSign] = str(df.velocity[numOfSrtangeSign]).replace('�', '0')
+                lineswithstrangesign.append(numOfSrtangeSign)
         numOfSrtangeSign += 1
 
-    result_df = pd.DataFrame({'intensity':df.intensity, 'velocity': df.velocity }).astype('float64')
-    result_df['density'] = result_df.intensity / result_df.velocity
 
+    print( 'lines with �-signs are: ', lineswithstrangesign)
+
+# создаем новый датафрейм с столбцом плотности потока
+    dens_df = pd.DataFrame({'intensity':df.intensity, 'velocity': df.velocity}).astype('float64')
+    dens_df['density'] = dens_df.intensity / dens_df.velocity
+
+    for i in lineswithstrangesign:
+        dens_df.velocity[i] = int(dens_df['velocity'].mean())
+
+# аномалии
+
+# аномалия, когда машины были, но скорость зафиксирована 0
     counterOfIssues1 = 0
     numOfLine = 0
     numNanLines = []
     lines_with_issues1 = []
-    for i in result_df.density:
+    for i in dens_df.density:
         if np.isnan(i):
            numNanLines.append(numOfLine)
 
@@ -30,20 +42,22 @@ def main():
            counterOfIssues1 += 1
            lines_with_issues1.append(numOfLine)
         numOfLine += 1
-        
+
+# аномалия, когда машин не было, но скорость зафиксированная не равна 0
     counterOfIssues2 = 0
     lines_with_issues2 = []
     newnumOfLine = 0
-    for i in result_df.intensity:
-        if int(result_df.intensity[newnumOfLine]) == 0: #and result_df.velocity[newnumOfLine] != 0.0:
+    for j in dens_df.intensity:
+        if int(dens_df.intensity[newnumOfLine]) == 0:
             counterOfIssues2 += 1
             lines_with_issues2.append(newnumOfLine)
-    numOfLine += 1
+        newnumOfLine += 1
 
 
-    print("The amount of 1st type - issues (intensity != 0 and velocity == 0 ) is", counterOfIssues1,'. lines are', lines_with_issues1)
-    print("The amount of 2nd type - issues  (intensity == 0 and velocity != 0 ) is", counterOfIssues2,'. lines are', lines_with_issues2)
+    print("The amount of 1st type - issues (intensity != 0 and velocity == 0 ) is", counterOfIssues1, '. lines are', lines_with_issues1)
+    print("The amount of 2nd type - issues  (intensity == 0 and velocity != 0 ) is", counterOfIssues2, '. lines are', lines_with_issues2)
+    print(dens_df['velocity'].head(9106))
 
-    print(result_df.tail(1000))
+
 if __name__ == "__main__":
     main()
--- new_test.py
+++ new_test.py
@@ -2,11 +2,5 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('zoo.csv', delimiter=',')
-counter = 0
-my_list = []
-for i in df.animal:
-    if df.animal[counter] == 'zebra' and df.uniq_id[counter] == 1011:
-        my_list.append(counter)
-    counter += 1
-print(my_list)
 
+print(df.uniq_id[1])
